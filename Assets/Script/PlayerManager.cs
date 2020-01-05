0463375657b64afb91b712271386096dfb1dbd79
nguyentienhoang <nguyentienhoang810@gmail.com>
2020-01-05 16:16:05 +0900

    add running, jumping, falling animation

---
+++ Assets/Script/PlayerManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    Animator animator;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        animator.SetBool("ifFalling", true);
+    }
+
+    private void FixedUpdate() {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Jump() {
+        Debug.Log("Jump");
+    }
+
+    public void Attack() {
+        Debug.Log("Attack");
+    }
+}
