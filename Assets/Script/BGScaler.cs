a45638a60fdb541e784c4270ae6fe7dcea995cfc
nguyentienhoang <nguyentienhoang810@gmail.com>
2020-01-05 14:11:25 +0900

    create char, background, auto left movement

---
+++ Assets/Script/BGScaler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class BGScaler : MonoBehaviour
+{
+    GameObject mainCamObj;
+    Camera cam;
+    float bgWidth;
+    SpriteRenderer bgRender;
+
+    private void Awake() {
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        bgRender = GetComponent<SpriteRenderer>();
+        Vector3 initScale = new Vector3(1, 1, 0);
+        Vector3 tempScale = transform.localScale;
+        transform.position = new Vector3(0, 0, 0);
+        transform.localScale = initScale;
+    }
+
+    void Start()
+    {
+        bgWidth = bgRender.bounds.size.x;
+        float bgHeight = bgRender.bounds.size.y;
+        float worldHeight = cam.orthographicSize * 2f;
+        float worldWidth = worldHeight * Screen.width / Screen.height;
+
+        Vector3 scrScale = new Vector3(1, worldHeight/bgHeight, 0);
+        transform.localScale = scrScale;
+    }
+}
---
+++ Assets/Script/Moving.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    public float speed;
+    
+    private void FixedUpdate() {
+        Vector3 temp = transform.position;
+        temp.x -= speed * Time.deltaTime;
+        transform.position = temp;
+    }
+}
