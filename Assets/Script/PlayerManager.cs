afae2bf3ac05ee25efb0e849db5e90c7d4f3e3fa
nguyentienhoang <nguyentienhoang810@gmail.com>
2020-01-05 17:02:41 +0900

    make jump animation better

--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -3,19 +3,32 @@
 public class PlayerManager : MonoBehaviour
 {
 
-    Animator animator;
+    private Animator animator;
+    private Rigidbody2D playerBody;
+    private float jumpPower = 600;
+    private bool isJumping = false;
+
+    public float jumpVelocity;
+    public float fall = 2.5f;
 
     private void Awake() {
+        playerBody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
-    void Start()
-    {
+    void Start() {
         animator.SetBool("ifFalling", true);
     }
 
     private void FixedUpdate() {
-        
+        if (isJumping) {
+            animator.SetBool("isJumping", true);
+            playerBody.velocity = Vector2.up * jumpVelocity;
+            isJumping = false;
+        }
+        if (playerBody.velocity.y < 0) {
+            playerBody.velocity += Vector2.up * Physics2D.gravity.y * (fall - 1) * Time.deltaTime;
+        }
     }
 
     void Update()
@@ -23,8 +36,14 @@ public class PlayerManager : MonoBehaviour
         
     }
 
+    private void OnCollisionEnter2D(Collision2D info) {
+        if (info.gameObject.name == "Tilemap") {
+            animator.SetBool("isRunning", true);
+        }
+    }
+
     public void Jump() {
-        Debug.Log("Jump");
+        isJumping = true;
     }
 
     public void Attack() {
