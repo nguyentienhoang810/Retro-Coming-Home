04b24302cea79c9e7ce135104f922fd9a5767377
nguyentienhoang <nguyentienhoang810@gmail.com>
2020-01-27 17:41:15 +0900

    rename class Moving -> Scrolling

--- Assets/Script/Scrolling.cs
+++ Assets/Script/Scrolling.cs
@@ -1,16 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Moving : MonoBehaviour
+public class Scrolling : MonoBehaviour
 {
     public float speed;
     
     private void FixedUpdate() {
+        Application.targetFrameRate = 60;
         Vector3 temp = transform.position;
         temp.x -= speed * Time.deltaTime;
         transform.position = temp;
     }
+
+    //infinity loop for background
 
     public void stopMoving() {
         
