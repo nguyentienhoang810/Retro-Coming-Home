735f882c764a97a1ea2adeb4bb795390fe1545c8
nguyentienhoang <nguyentienhoang810@gmail.com>
2020-01-30 17:25:09 +0900

    fix enemy move script

--- Assets/Script/EnemyMove.cs
+++ Assets/Script/EnemyMove.cs
@@ -3,9 +3,15 @@
 public class EnemyMove : MonoBehaviour
 {
     private Vector3 tempPos;
-    private float leftDistance = 20;
+    public float leftDistance = 20;
+    public int speed = 2;
     private float rightDistance;
     private bool isMoveLeft = true;
+
+    private enum DIRECTION_TYPE {
+        LEFT,
+        RIGHT
+    }
     private void Awake() {
         rightDistance = leftDistance;
         tempPos = transform.position;
@@ -19,8 +25,7 @@ public class EnemyMove : MonoBehaviour
 
     }
     private void FixedUpdate() {
-        tempPos.x -= 5 * Time.deltaTime;
-        transform.position = tempPos;
+        Application.targetFrameRate = 60;
         if (isMoveLeft == true) {
             moveLeft();
         } else {
@@ -31,8 +36,9 @@ public class EnemyMove : MonoBehaviour
     private void moveLeft() {
         if (leftDistance > 0) {
             //on moving to left
+            transformDirection(DIRECTION_TYPE.LEFT);
             leftDistance -= 0.2f;
-            tempPos.x -= 5 * Time.deltaTime;
+            tempPos.x -= speed * Time.deltaTime;
             transform.position = tempPos;
         } else {
             isMoveLeft = false;
@@ -43,12 +49,24 @@ public class EnemyMove : MonoBehaviour
     private void moveRight() {
         if(rightDistance > 0) {
             //on moving right
+            transformDirection(DIRECTION_TYPE.RIGHT);
             rightDistance -= 0.2f;
-            tempPos.x += 5 * Time.deltaTime;
+            tempPos.x += speed * Time.deltaTime;
             transform.position = tempPos;
         } else {
             isMoveLeft = true;
             leftDistance = 20;
+        }
+    }
+
+    private void transformDirection(DIRECTION_TYPE direction) {
+        switch(direction) {
+            case DIRECTION_TYPE.LEFT:
+            transform.localScale = new Vector3(1,1,1);
+            break;
+            case DIRECTION_TYPE.RIGHT:
+            transform.localScale = new Vector3(-1,1,1);
+            break;
         }
     }
 }
