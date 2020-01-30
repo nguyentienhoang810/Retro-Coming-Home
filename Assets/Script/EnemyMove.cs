006775cbf284ea202b4ccb0a7c163257b7494d3f
nguyentienhoang <nguyentienhoang810@gmail.com>
2020-01-30 17:04:19 +0900

    add scrolling for player and main cam

---
+++ Assets/Script/EnemyMove.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    private Vector3 tempPos;
+    private float leftDistance = 20;
+    private float rightDistance;
+    private bool isMoveLeft = true;
+    private void Awake() {
+        rightDistance = leftDistance;
+        tempPos = transform.position;
+    }
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+
+    }
+    private void FixedUpdate() {
+        tempPos.x -= 5 * Time.deltaTime;
+        transform.position = tempPos;
+        if (isMoveLeft == true) {
+            moveLeft();
+        } else {
+            moveRight();
+        }
+    }
+
+    private void moveLeft() {
+        if (leftDistance > 0) {
+            //on moving to left
+            leftDistance -= 0.2f;
+            tempPos.x -= 5 * Time.deltaTime;
+            transform.position = tempPos;
+        } else {
+            isMoveLeft = false;
+            rightDistance = 20;
+        }
+    }
+
+    private void moveRight() {
+        if(rightDistance > 0) {
+            //on moving right
+            rightDistance -= 0.2f;
+            tempPos.x += 5 * Time.deltaTime;
+            transform.position = tempPos;
+        } else {
+            isMoveLeft = true;
+            leftDistance = 20;
+        }
+    }
+}
--- Assets/Script/Scrolling.cs
+++ Assets/Script/Scrolling.cs
@@ -3,25 +3,10 @@
 public class Scrolling : MonoBehaviour
 {
     public float speed;
-    private float bgWidth;
-    private float scrWidth;
-    
-    private GameObject bg;
-    private GameObject bg2;
-    private Vector3 initPos;
-    private SpriteRenderer bgRender;
-    Camera cam;
-
     private float startX = 23.5f;
-
+    private Camera cam;
     private void Awake() {
         cam = GameObject.Find("Main Camera").GetComponent<Camera>();
-        bg = GameObject.FindGameObjectWithTag("background");
-        bg2 = GameObject.FindGameObjectWithTag("background2");
-        bgRender = bg.GetComponent<SpriteRenderer>();
-        bgWidth = bgRender.bounds.size.x;
-        scrWidth = calculateScrWidth();
-        initPos = bg2.transform.localPosition;
     }
 
     private void FixedUpdate() {
@@ -29,29 +14,6 @@ public class Scrolling : MonoBehaviour
         Vector3 temp = transform.position;
         temp.x -= speed * Time.deltaTime;
         transform.position = temp;
-        genBackground();
-    }
-
-    //infinity loop for background
-    private void genBackground() {
-        float xTemp = bg.transform.position.x;
-        if (xTemp < -((bgWidth / 2 + scrWidth / 2) + (bgWidth / 2 - scrWidth / 2))) {
-            // bg.transform.position = new Vector3(initPos.x, initPos.y, initPos.z);
-            bg.transform.position = new Vector3(startX, initPos.y, initPos.z);
-        }
-
-        float x2Temp = bg2.transform.position.x;
-        if (x2Temp < -((bgWidth / 2 + scrWidth / 2) + (bgWidth / 2 - scrWidth / 2))) {
-            // bg2.transform.position = new Vector3(initPos.x, initPos.y, initPos.z);
-            bg2.transform.position = new Vector3(startX, initPos.y, initPos.z);
-        }
-    }
-
-    private float calculateScrWidth() {
-        float bgHeight = bgRender.bounds.size.y;
-        float scrHeight = cam.orthographicSize * 2f;
-        float scrWidth = scrHeight * Screen.width / Screen.height;
-        return scrWidth;
     }
 
     public void stopMoving() {
