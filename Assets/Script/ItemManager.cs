02794bffc346541406f1384814dc0ec2a8699c31
nguyentienhoang <nguyentienhoang810@gmail.com>
2020-02-08 15:52:23 +0900

    get item animation

--- Assets/Script/ItemManager.cs
+++ Assets/Script/ItemManager.cs
@@ -3,7 +3,9 @@
 public class ItemManager : MonoBehaviour
 {
     public int gemScore = 10;
+    [SerializeField] GameObject getItemAnimation;
     public void destroy() {
+        Instantiate(getItemAnimation, transform.position, transform.rotation);
         Destroy(this.gameObject);
     }
 }
